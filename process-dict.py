acc5c2f2aba4dc53c7623dda9b03320d20e442d8
Alexander Yakovlev <33040934+AlexJameson@users.noreply.github.com>
2023-12-07 15:52:25 +0300

    add gitignore and refactor noun processing

--- process-dict.py
+++ process-dict.py
@@ -6,6 +6,22 @@ input_file = 'dict-a.txt'
 
 output_file = 'test-output.txt'
 
+# Grammatical number = NMbr as per OpenCorpora
+numbers = ('sing', 'plur')
+
+genders = ('masc', 'femn', 'neut')
+
+# Case = CAse as per OpenCorpora
+cases = ('nomn', 'gent', 'datv', 'accs', 'ablt', 'loct')
+
+# Verb tense = TEns as per OpenCorpora
+Tenses = ('pres', 'past', 'futr')
+
+# Verb person = PERs as per OpenCorpora
+Persons = ('1per', '2per', '3per')
+
+# Use MOod == 'impr' to conjugate verbs too
+
 # Open the output file in write mode
 with open(output_file, 'w') as file_out:
     # Process each word and write the result to the common output file
@@ -16,47 +32,15 @@ with open(output_file, 'w') as file_out:
             processed_word = morph.parse(word)[0]
 
             if processed_word.tag.POS == 'NOUN':
-                nomn = processed_word.inflect({'sing', 'nomn'}).word
-                file_out.write(nomn + '\n')
-
-                gent = processed_word.inflect({'sing', 'gent'}).word
-                file_out.write(gent + '\n')
-
-                datv = processed_word.inflect({'sing', 'datv'}).word
-                file_out.write(datv + '\n')
-
-                accs = processed_word.inflect({'sing', 'accs'}).word
-                file_out.write(accs + '\n')
-
-                ablt = processed_word.inflect({'sing', 'ablt'}).word
-                file_out.write(ablt + '\n')
-
-                loct = processed_word.inflect({'sing', 'loct'}).word
-                file_out.write(loct + '\n')
-
-                plural_noun = processed_word.inflect({'plur'})
-
-                if plural_noun:
-                    plural_nomn = plural_noun.inflect({'nomn'}).word
-                    file_out.write(plural_nomn + '\n')
-
-                    plural_nomn = plural_noun.inflect({'gent'}).word
-                    file_out.write(plural_nomn + '\n')
-
-                    plural_nomn = plural_noun.inflect({'datv'}).word
-                    file_out.write(plural_nomn + '\n')
-
-                    plural_nomn = plural_noun.inflect({'accs'}).word
-                    file_out.write(plural_nomn + '\n')
-
-                    plural_nomn = plural_noun.inflect({'ablt'}).word
-                    file_out.write(plural_nomn + '\n')
-
-                    plural_nomn = plural_noun.inflect({'loct'}).word
-                    file_out.write(plural_nomn + '\n')
+                if processed_word:
+                    for num in numbers:
+                        for case in cases:
+                            noun = processed_word.inflect({num, case})
+                            if noun:
+                                file_out.write(noun.word + '\n')
 
                 else:
-                    file_out.write('Failed to inflect to plural')
+                    file_out.write('Failed to conjugate')
 
             elif processed_word.tag.POS == 'INFN':
                 # Process infinitive verbs
